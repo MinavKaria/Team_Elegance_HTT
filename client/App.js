ea38583c1ce90bac90e551d26c57c6d8d97c8573
Harsh <harshnagrani009@gmail.com>
2024-03-23 16:13:54 +0530

    Budeget page

--- client/App.js
+++ client/App.js
@@ -3,18 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>HTT2.0</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: 'white', alignItems: 'center', justifyContent: 'center' }}>
+      <View>
+        <Text style={{ color: 'black', fontSize: 22, fontWeight:900, right: '35%', bottom:'1100%'}}>ZORKO</Text>
+      </View>
+      <View>
+        <Text style={{color:'black', fontWeight:'bold', fontSize:40}}>Control your <Text style={{color:'#FF8E5E'}}>Budget</Text> stress-free</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
