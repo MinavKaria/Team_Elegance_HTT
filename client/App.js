88fffeb9a5f34513db7ca293b14a4d2a0dbee839
aayushmehta02 <83535280+aayushmehta02@users.noreply.github.com>
2024-03-23 16:17:51 +0530

    HOME SCREEN AND IMAGES

--- client/App.js
+++ client/App.js
@@ -1,13 +1,24 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import Home from './components/Home';
 const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Zorko"  options={{
+            headerTitle: () => (
+              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <Image
+                source={require('../client/assets/images/logo.png')}
+                style={{ width: 100, height: 50 }}
+                resizeMode="contain"
+              />
+              <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Zorko</Text>
+              </View>
+              
+              ),}} component={Home} />
      
     </Stack.Navigator>
   </NavigationContainer>
