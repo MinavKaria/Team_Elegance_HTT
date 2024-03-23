44a8b872a5b4155af04496588aaa1987878cc393
MinavKaria <minavpkaria@gmail.com>
2024-03-24 02:49:55 +0530

    add server

---
+++ server_vendor/adminSchema.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const adminSchema = new mongoose.Schema({
+    id: Number,
+    name: String,
+    location: String,
+    foodMenu:[{
+        foodName:String,
+        quantity:Number,
+        price:Number
+    }],
+    DateOfJoining:Date,
+    phone:Number,
+    email:String,
+    age:Number,
+    upVotes:Number,
+    downVotes:Number,
+    password:String
+});
+
+const Admin = mongoose.model('Admin', adminSchema);
+
+export default Admin;
