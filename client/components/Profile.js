1753a020a3732ec82b3b63c560b752801b2efaf1
aayushmehta02 <83535280+aayushmehta02@users.noreply.github.com>
2024-03-24 11:13:50 +0530

    gift card

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -1,25 +1,65 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
-const Profile = () => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Profile Page</Text>
-        </View>
-    );
+const CreditCard = ({ cardNumber, cardHolder, expiryDate }) => {
+  return (
+    <View>
+    <Text style={styles.title}>MEMBERSHIP CARD</Text>
+    <View style={styles.container}>
+      <Image source={require('../assets/images/logo.png')} style={styles.cardImage} />
+      <Text style={styles.cardNumber}>XXXX &nbsp; XXXX &nbsp; XXXX &nbsp; 2345</Text>
+      <Text style={styles.cardHolder}>ZORKO PLATINUM MEMBERSHIP</Text>
+      
+    </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+  container: {
+    backgroundColor: '#222',
+    padding: 20,
+    borderRadius: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
     },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 16,
-    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+    elevation: 6,
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+    marginBottom: 10,
+    textDecorationLine: 'underline',
+    textAlign: 'center',
+  },
+  cardImage: {
+    alignSelf: 'center',
+    marginBottom: 20,
+    width: 100,
+    height: 50,
+  },
+  cardNumber: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 10,
+  },
+  cardHolder: {
+    fontSize: 16,
+    color: '#fff',
+    marginBottom: 5,
+    textTransform: 'uppercase',
+  },
+  expiryDate: {
+    fontSize: 16,
+    color: '#fff',
+  },
 });
 
-export default Profile;
+export default CreditCard;
